454e8799cbf8f08171f916d05cec991b0decb0d2
Alex Meier <alexandre_meier@bluewin.ch>
2021-06-19 15:07:36 +0100

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -26,11 +26,12 @@ tar -xf sudo-1.8.31p2.tar
 #START
 
 ## NORMAL COMPILATION!
+
+# echo "Copy Stuff"
 # cp -R sudo-1.8.31p2 /root/.
-
 # echo "Compiling Stuff"
-
 # cd /root/sudo-1.8.31p2 && ./configure && make && make install 
+
 
 # AFL Instrumentation Compilation!
 
@@ -38,7 +39,6 @@ echo "Copy Stuff"
 cp /root/code/episode02/argv-fuzz-inl.h /root/code/sudo-1.8.31p2/src/. 
 cp /root/code/episode02/progname.c /root/code/sudo-1.8.31p2/lib/util/.
 cp /root/code/episode02/sudo.c /root/code/sudo-1.8.31p2/src/.
-
 echo "Compiling Stuff"
 cp -R sudo-1.8.31p2 /root/.
 cd /root/sudo-1.8.31p2 && CC=/root/AFL/afl-clang-fast ./configure --disable-shared && make && make install
@@ -47,4 +47,4 @@ cd /root/sudo-1.8.31p2 && CC=/root/AFL/afl-clang-fast ./configure --disable-shar
 
 mkdir /tmp/in && mkdir /tmp/out && echo -en "sudo\x00-l\x00" > /tmp/in/1.testcase
 
-echo /root/sudo-1.8.31p2/src/sudo
+/root/sudo-1.8.31p2/src/sudo --version
